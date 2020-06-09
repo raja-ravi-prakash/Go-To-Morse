ee39b99198cadf1455a4d4ca2b9a7269dadecf8f
raja-ravi-prakash <iamraviprakash1@gmail.com>
2020-06-09 21:51:18 +0530

    ui

--- index.js
+++ index.js
@@ -2,25 +2,41 @@ const express = require("express");
 const socketIO = require("socket.io");
 const http = require("http");
 const axios = require("axios");
-let cors = require("cors");
-let body = require("body-parser");
+const ng = require("ngrok");
+const cors = require("cors");
+const body = require("body-parser");
 let app = express();
 let server = http.createServer(app);
 let io = socketIO(server);
 let handler = require("./core/DataHandler");
+let sc;
 
 const db = new handler.Handler();
 app.use(express.static("static"));
 app.use(body.json());
 app.use(cors());
 
+var url;
+
+(async () => {
+  try {
+    url = await ng.connect();
+    console.log(url);
+  } catch (e) {
+    console.log(e);
+  }
+})();
+
 io.on("connection", (socket) => {
+  console.log("Connected");
   let backUP = db.read();
+  socket.emit("global", url);
 
   if (!isNaN(backUP)) {
   } else {
     socket.emit("startup", backUP);
   }
+
   app.post("/msg", (req, res) => {
     let message = req.body;
     socket.emit("message", message);
--- static/core.js
+++ static/core.js
@@ -94,13 +94,51 @@ function send() {
   socket.emit("new", data);
 }
 
+function copyToClipboard(text) {
+  alert("Text Copied!!");
+  if (window.clipboardData && window.clipboardData.setData) {
+    return clipboardData.setData("Text", text);
+  } else if (
+    document.queryCommandSupported &&
+    document.queryCommandSupported("copy")
+  ) {
+    var textarea = document.createElement("textarea");
+    textarea.textContent = text;
+    textarea.style.position = "fixed";
+    document.body.appendChild(textarea);
+    textarea.select();
+    try {
+      return document.execCommand("copy");
+    } catch (ex) {
+      console.warn("Copy to clipboard failed.", ex);
+      return false;
+    } finally {
+      document.body.removeChild(textarea);
+    }
+  }
+}
+
 function add(data) {
   let block = document.getElementById("messages");
 
-  let msg = document.createElement("h2");
-  msg.innerHTML = decode(data.message);
+  let fromTo = document.createElement("h2");
+  fromTo.innerHTML = window.location.href == data.from ? "Me" : data.from;
 
-  block.append(msg);
+  let content = document.createElement("p");
+  content.className = "content";
+  content.innerHTML = decode(data.message);
+
+  let time = document.createElement("span");
+  time.className = "time";
+  time.innerHTML = data.date;
+
+  let msgBlock = document.createElement("div");
+  msgBlock.className = "block";
+  msgBlock.appendChild(fromTo);
+  msgBlock.appendChild(content);
+  msgBlock.appendChild(time);
+
+  block.appendChild(msgBlock);
 }
 
 socket.on("connect", function () {
@@ -112,5 +150,23 @@ socket.on("connect", function () {
 
   socket.on("message", (data) => {
     add(data);
+  });
+
+  socket.on("global", (data) => {
+    let link = document.createElement("h3");
+    link.className = "link";
+    link.innerHTML = data + "/msg";
+
+    let heading = document.createElement("div");
+    heading.className = "heading";
+    heading.onclick = () => {
+      let data = link.innerHTML;
+      copyToClipboard(data);
+    };
+    heading.appendChild(link);
+
+    document
+      .getElementsByTagName("body")[0]
+      .insertBefore(heading, document.getElementById("actions"));
   });
 });
